e3b5fdbf73f8b380a3d33e6f6b7c52557d34e2f7
kevin-d-omara <kevin.d.omara@gmail.com>
2016-07-09 18:49:27 -0700

    added Ball class; child of GameObject

---
+++ Ball.lua
@@ -0,0 +1,41 @@
+-- TODO: make collisions based on angle/edges (i.e. original Pong)
+--       ^ include varying y-force based on this too
+
+-- define ball object; child of game object
+Ball = GameObject:new()
+table.remove(allGameObjects)  -- don't want prototype in object list
+
+function Ball:new(x, y, speedx, speedy, maxspeedx, maxspeedy, width, height, color)
+    local obj = GameObject:new(x, y, speedx, speedy, maxspeedx, maxspeedy, width, height, color)
+    
+    setmetatable(obj,self)
+    self.__index = self
+    return obj
+end
+
+
+function Ball:spawnBall()
+    local initSpeedX = 300 * (math.random() < 0.5 and 1 or -1)
+    local initSpeedY = math.random() < 0.5 and math.random(25,150) or math.random(-25,-150)
+    ball = self:new(window.width/2-5, window.height/2-5, initSpeedX, initSpeedY, 300, 450, 12, 12, {51,204,51,255})
+end
+Ball:spawnBall()
+
+--[[
+function Ball:draw()    -- overwrite parent default: rectangle
+    love.graphics.setColor(self.color[1],self.color[2],self.color[3],self.color[4])
+    love.graphics.circle("fill", self.x, self.y, self.height/2, 50)
+end
+--]]
+
+function Ball.bounce()
+    --local bassBounce = love.audio.newSource("sounds/deep-bass-bounce.wav", "static")
+    local bassBounce = love.audio.newSource("sounds/ball-bounce.mp3", "static")
+    table.insert(soundStack, bassBounce)
+end
+
+function Ball.score()
+    local pointScored = love.audio.newSource("sounds/point-scored.wav", "static")
+    pointScored:setPitch(3)
+    table.insert(soundStack, pointScored)
+end
