320bed12dd158c771620c5fe85edbae0cf43f1b3
kevin-d-omara <kevin.d.omara@gmail.com>
2016-07-09 18:51:10 -0700

    added new fonts for menu

--- config.lua
+++ config.lua
@@ -5,5 +5,9 @@ love.window.setMode(600, 400)
 window = {}
 window.width, window.height, window.flags = love.window.getMode()
 
-scoreFont = love.graphics.newFont(36)
+scoreFont = love.graphics.newFont(36)   -- TODO: remove
+titleFont = love.graphics.newFont(36)
+optionFont = love.graphics.newFont(28)
+subOptionFont = love.graphics.newFont(20)
+
 love.graphics.setFont(scoreFont)
