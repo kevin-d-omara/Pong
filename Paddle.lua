5d71799a51fb7f5ecb15d9274d8807ebc3b9b74e
kevin-d-omara <kevin.d.omara@gmail.com>
2016-07-09 18:49:39 -0700

    added Paddle class; child of GameObject

---
+++ Paddle.lua
@@ -0,0 +1,15 @@
+Paddle = GameObject:new()
+table.remove(allGameObjects)  -- don't want prototype in object list
+
+function Paddle:new(x, y, speedx, speedy, maxspeedx, maxspeedy, width, height, color)
+    local obj = GameObject:new(x, y, speedx, speedy, maxspeedx, maxspeedy, width, height, color)
+    obj.bassBounce = love.audio.newSource("sounds/deep-bass-bounce.wav", "static")
+    
+    setmetatable(obj,self)
+    self.__index = self
+    return obj
+end
+
+function Paddle:bounceWall()
+    table.insert(soundStack, self.bassBounce)
+end
