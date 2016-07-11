cc9e4b83b6fa438e7d29aa17965f8ba4ae17a87f
kevin-d-omara <kevin.d.omara@gmail.com>
2016-07-10 20:45:11 -0700

    moved random seed initialization to here

--- config.lua
+++ config.lua
@@ -1,13 +1,13 @@
 love.window.setTitle("Pong")
-
 love.window.setMode(600, 400)
-
 window = {}
 window.width, window.height, window.flags = love.window.getMode()
+
+math.randomseed( tonumber(tostring(os.time()):reverse():sub(1,6)) )
+love.keyboard.setKeyRepeat(true)
 
 scoreFont = love.graphics.newFont(36)   -- TODO: remove
 titleFont = love.graphics.newFont(36)
 optionFont = love.graphics.newFont(28)
 subOptionFont = love.graphics.newFont(20)
-
 love.graphics.setFont(scoreFont)
