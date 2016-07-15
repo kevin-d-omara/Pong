524460030189ec4c2c595de72b2121c80a87093e
kevin-d-omara <kevin.d.omara@gmail.com>
2016-07-15 13:14:22 -0700

    most relevant variables now intrinsic to paddle
    also enhanced modularity -> paddle always spawns with a 10 pixel gap
    from the left or right edge

--- Paddle.lua
+++ Paddle.lua
@@ -1,8 +1,13 @@
 Paddle = GameObject:new()
 table.remove(allGameObjects)  -- don't want prototype in object list
 
-function Paddle:new(x, y, speedx, speedy, maxspeedx, maxspeedy, width, height, color)
-    local obj = GameObject:new(x, y, speedx, speedy, maxspeedx, maxspeedy, width, height, color)
+--window.width-18
+
+function Paddle:new(side, color)
+    local width = 15
+    local gap = 10
+    local x = side == "left" and (gap) or (window.width - gap - width)
+    local obj = GameObject:new(x, window.height/2-30, 0, 0, 0, 475, width, 60, color)
     obj.bassBounce = love.audio.newSource("sounds/deep-bass-bounce.wav", "static")
     
     setmetatable(obj,self)
